0576fcd2263dc4e34d34854f51f292e3f07bd5c8
Desert Python <mattsenneff@aol.com>
2020-08-25 20:44:01 -0700

    logout and login

--- server.js
+++ server.js
@@ -7,3 +7,18 @@ app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.static(path.join(__dirname, 'public')));
+const session = require('express-session');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
