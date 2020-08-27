57054d365c1b38e297a97f6c90febfec5dda4770
Desert Python <mattsenneff@aol.com>
2020-08-26 19:12:23 -0700

    intro to {{if}}

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -87,12 +87,15 @@ router.get('/post/:id', (req, res) => {
       const post = dbPostData.get({ plain: true });
 
       // pass data to template
-      res.render('single-post', { post });
+    res.render('single-post', {
+        post,
+        loggedIn: req.session.loggedIn
     })
     .catch(err => {
       console.log(err);
       res.status(500).json(err);
     });
 });
+
 
 module.exports = router;
